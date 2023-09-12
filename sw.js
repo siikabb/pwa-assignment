15833a2744a8411b8fbb6313b34c140878ef8495
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-09-12 10:59:36 +0300

    add core app

---
+++ sw.js
@@ -0,0 +1,35 @@
+const cacheName = 'hello-pwa';
+const filesToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js',
+  './fonts/Quicksand-Regular.ttf',
+  './img/bg.jpeg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    (async () => {
+      try {
+        const cache = await caches.open(cacheName);
+        return cache.addAll(filesToCache);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })()
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    (async () => {
+      try {
+        const response = await caches.match(e.request);
+        return response || fetch(e.request);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })()
+  );
+});
